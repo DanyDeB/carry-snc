c565e698181b97c780131d18f8eeb98a5236b2a3
DanyDeB <de.brito.ln@gmail.com>
2022-06-23 16:58:33 +0200

    HEADER
    Update css for header, specific header for index page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -34,7 +34,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 function scrollHeader(){
     const header = document.getElementById('header')
     // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
